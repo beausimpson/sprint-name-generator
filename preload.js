f9d5c107c8c1f9cfbedda838f547dfdb9225173e
beausimpson <simpson.beau@gmail.com>
2020-01-15 22:07:15 -0600

    bootstrap & jquery not working

--- preload.js
+++ preload.js
@@ -1,7 +1,9 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
+  require('jquery');
+  
+  const replaceText = (selector, text) => {
       const element = document.getElementById(selector)
       if (element) element.innerText = text
     }
@@ -9,5 +11,17 @@ window.addEventListener('DOMContentLoaded', () => {
     for (const type of ['chrome', 'node', 'electron']) {
       replaceText(`${type}-version`, process.versions[type])
     }
-  })
+
+    document.getElementById("name_generator_button").addEventListener("click", generateName);
+
+    function generateName() {
+      console.log("test")  
+    }
+
+    // $("#name_generator_button").on("click", function (event) 
+    // {event.preventDefault();
+    // console.log("test")
+    // });
+
+});
   
