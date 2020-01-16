ae58d67a2274ef625653e133cd37ea6ad3ffb7b4
beausimpson <simpson.beau@gmail.com>
2020-01-16 16:42:33 -0600

    started on ajax call

--- preload.js
+++ preload.js
@@ -1,18 +1,30 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  
-  let $ = require("jquery")
-  window.Bootstrap = require('bootstrap')
-  
-  
+    // All of the Node.js APIs are available in the preload process.
+    // It has the same sandbox as a Chrome extension.
+    window.addEventListener('DOMContentLoaded', () => {
+    
+    let $ = require("jquery")
+    // window.Bootstrap = require('bootstrap')
 
-  
-
+    
+    
+    
+    
     $("#name_generator_button").on("click", function (event) {
-      event.preventDefault();
-    console.log("test")
+        event.preventDefault();
+        
+        var sprintNumber = $("#sprint_number").val().trim();
+        var searchTerm = $("#search_term").val().trim();
+
+        
+        $("#generated_sprint_names").html(`<h5>${sprintNumber}</h5><p>${searchTerm}</p>`);
+
+        // console.log(sprintNumber + searchTerm);
+
+        $("#sprint_number").val("");
+        $("#search_term").val("");
+    
     });
 
+
 });
-  
+    
